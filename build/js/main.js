2a3d3019870cbecc32717b58b5c319f22167c157
Azune-NK <w1ndy@inbox.ru>
2020-01-24 13:52:26 +0300

    Добавляет скрипт плавного скрола

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,27 @@
 
 'use strict';
 
+var scrollBtn = document.querySelector('.intro__scrolldown');
+var advisoryBtn = document.querySelector('.intro__advisory-btn');
+var infoNode = document.querySelector('.info');
+var advisoryNode = document.querySelector('.advisory');
+
+var moveTo = new MoveTo();
+
+if (scrollBtn && infoNode) {
+  scrollBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    moveTo.move(infoNode);
+  });
+}
+
+if (advisoryBtn && advisoryNode) {
+  advisoryBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    moveTo.move(advisoryNode);
+  });
+}
+
 'use strict';
 
 var telInputs = document.querySelectorAll('input[type="tel"]');
