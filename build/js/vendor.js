e63de64f118e30285110a4fe537e2d6176da2f6b
Azune-NK <w1ndy@inbox.ru>
2020-01-26 19:09:54 +0300

    Добавляет fix формата img в блоке товаров + foreach polyfill

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -1,3 +1,7 @@
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 /*!
  * dist/inputmask
  * https://github.com/RobinHerbots/Inputmask
